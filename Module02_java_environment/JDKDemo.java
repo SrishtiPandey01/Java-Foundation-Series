5276ffdecee228acdd771c4f6d7159d9d0b3a2ec
SrishtiPandey01 <srishtipandey20052005@gmail.com>
2026-09-23 19:44:58 +0530

    Adding Java environment program

---
+++ Module02_java_environment/JDKDemo.java
@@ -0,0 +1,6 @@
+public class JDKDemo {
+    public static void main(String[] args) {
+        System.out.println("JDK is used to develop Java applications.");
+        System.out.println("JDK is a software development kit that provides tools and libraries for compiling, debugging, and running Java programs."); 
+    }
+}
