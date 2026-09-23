945aa24095e787a24b16c0471e97119022181032
SrishtiPandey01 <srishtipandey20052005@gmail.com>
2026-09-23 19:48:45 +0530

    Adding JRE Program

---
+++ Module02_java_environment/JREDemo.java
@@ -0,0 +1,8 @@
+public class JREDemo {
+    public static void main(String[] args) {
+        System.out.println("JRE is used to run Java applications.");
+        System.out.println(
+                "JRE is a software package that provides the necessary runtime environment for executing Java programs, including the Java Virtual Machine (JVM) and core libraries.");
+    }
+
+}
