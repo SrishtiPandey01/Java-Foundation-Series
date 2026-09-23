03b8c5b10b6b9dcbef953c6dccae31c626a64f44
SrishtiPandey01 <srishtipandey20052005@gmail.com>
2026-09-23 20:24:43 +0530

    Adding Programs

---
+++ Module02_java_environment/JVMDemo.java
@@ -0,0 +1,7 @@
+public class JVMDemo {
+    public static void main(String[] args) {
+        System.out.println("JVM is used to execute Java bytecode.");
+        System.out.println(
+                "JVM is an abstract computing machine that enables a computer to run Java programs by converting bytecode into machine code and executing it.");
+    }
+}
---
+++ Module02_java_environment/JavaProgramStructure.java
@@ -0,0 +1,8 @@
+public class JavaProgramStructure {
+    public static void main(String[] args) {
+        System.out.println("Java Program Structure:");
+        System.out.println("1. Class Declaration: The class is declared using the 'class' keyword followed by the class name.");
+        System.out.println("2. Main Method: The main method is the entry point of the program and is defined as 'public static void main(String[] args)'.");
+        System.out.println("3. Statements: Inside the main method, you can write statements to perform actions, such as printing to the console.");
+    }   
+}
